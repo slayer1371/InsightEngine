244a3a1337a705c08d5bc23315d1079308d8c6e5
Mrinal Sharma <mrinalwork2@gmail.com>
2026-01-17 22:36:21 -0500

    suspense for useSearchParams

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "@/components/login-form"
+import { Suspense } from "react"
 
 export default function LoginPage() {
   return (
@@ -12,7 +13,9 @@ export default function LoginPage() {
           </div>
           InsightEngine Inc.
         </a>
+        <Suspense fallback={<div>Loading form...</div>}>
         <LoginForm />
+      </Suspense>
       </div>
     </div>
   )
